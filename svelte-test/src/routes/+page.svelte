72006cfeeb2116e7ee5c4385d6a9cd739b6cbce5
Ravi <108709054+rkvmar@users.noreply.github.com>
2025-02-14 13:59:43 -0800

    svelt

---
+++ svelte-test/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let numbers = $state([1]);
+    let sum = $derived(numbers.reduce((sum, n) => sum + n, 0));
+	function addNumber() {
+		numbers.push(numbers.length + 1)
+	}
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
